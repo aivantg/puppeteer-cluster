07c477fd3a3f992a9a477a22790379f21ca47c47
aivantg <aivant.developer@gmail.com>
2021-09-09 23:48:00 -0700

    Added streaming to browser launch

--- src/concurrency/built-in/Browser.ts
+++ src/concurrency/built-in/Browser.ts
@@ -1,4 +1,3 @@
-
 import * as puppeteer from 'puppeteer';
 
 import { debugGenerator, timeoutExecute } from '../../util';
@@ -6,6 +5,9 @@ import ConcurrencyImplementation, { WorkerInstance } from '../ConcurrencyImpleme
 const debug = debugGenerator('BrowserConcurrency');
 
 const BROWSER_TIMEOUT = 5000;
+
+// @ts-ignore
+declare const Buffer
 
 export default class Browser extends ConcurrencyImplementation {
     public async init() {}
@@ -16,6 +18,36 @@ export default class Browser extends ConcurrencyImplementation {
 
         const options = perBrowserOptions || this.options;
         let chrome = await this.puppeteer.launch(options) as puppeteer.Browser;
+
+        chrome.encoders = new Map();
+
+        const extensionTarget = await chrome.waitForTarget(
+            // @ts-ignore
+            (target) => target.type() === "background_page" && target._targetInfo.title === "Video Capture"
+        );
+    
+        // @ts-ignore
+        chrome.videoCaptureExtension = await extensionTarget.page();
+
+        const str2ab = (str: any) => {
+            // Convert a UTF-8 String to an ArrayBuffer
+        
+            var buf = new ArrayBuffer(str.length); // 1 byte for each char
+            var bufView = new Uint8Array(buf);
+        
+            for (var i = 0, strLen = str.length; i < strLen; i++) {
+                bufView[i] = str.charCodeAt(i);
+            }
+            return buf;
+        }
+    
+        // @ts-ignore
+        await chrome.videoCaptureExtension.exposeFunction("sendData", (opts: any) => {
+            const data = Buffer.from(str2ab(opts.data));
+            // @ts-ignore
+            chrome.encoders.get(opts.id).push(data);
+        });
+
         let page: puppeteer.Page;
         let context: any; // puppeteer typings are old...
 
@@ -50,6 +82,34 @@ export default class Browser extends ConcurrencyImplementation {
 
                 // just relaunch as there is only one page per browser
                 chrome = await this.puppeteer.launch(this.options);
+                chrome.encoders = new Map();
+
+                const extensionTarget = await chrome.waitForTarget(
+                    // @ts-ignore
+                    (target) => target.type() === "background_page" && target._targetInfo.title === "Video Capture"
+                );
+            
+                // @ts-ignore
+                chrome.videoCaptureExtension = await extensionTarget.page();
+
+                const str2ab = (str: any) => {
+                    // Convert a UTF-8 String to an ArrayBuffer
+                
+                    var buf = new ArrayBuffer(str.length); // 1 byte for each char
+                    var bufView = new Uint8Array(buf);
+                
+                    for (var i = 0, strLen = str.length; i < strLen; i++) {
+                        bufView[i] = str.charCodeAt(i);
+                    }
+                    return buf;
+                }
+            
+                // @ts-ignore
+                await chrome.videoCaptureExtension.exposeFunction("sendData", (opts: any) => {
+                    const data = Buffer.from(str2ab(opts.data));
+                    // @ts-ignore
+                    chrome.encoders.get(opts.id).push(data);
+                });
             },
         };
     }
